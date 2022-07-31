1ad0e84ede4eef44b2f7f707d1ba4d3f15439d54
Mikhail Zaborov <zaborovm@gmail.com>
2022-07-31 12:42:20 +0300

    after 8. BeanFactoryPostProcessor (BFPP)

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -2,17 +2,25 @@ package com.dmdev.spring;
 
 import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CompanyRepository;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.Serializable;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        CompanyRepository companyRepository;
+        String value = "hello";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanPostProcessor.class.isAssignableFrom(value.getClass()));
+        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+
+
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml")) {
             //      clazz -> String -> Map<String, Object>
             ConnectionPool connectionPool = context.getBean("p1", ConnectionPool.class);
             System.out.println(connectionPool);
 
-            companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+            CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
             System.out.println(companyRepository);
         }
 
