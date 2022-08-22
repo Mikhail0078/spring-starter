294a1cb404f90f702a323e8eba29ce492468c9b5
Mikhail Zaborov <zaborovm@gmail.com>
2022-08-22 20:20:24 +0300

    after 4.5. Profiles

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Component;
 
+
 //@ImportResource("classpath:application.xml")
 @Import(WebConfiguration.class)
 @Configuration(proxyBeanMethods = true)
@@ -20,7 +21,6 @@ import org.springframework.stereotype.Component;
                 @Filter(type = FilterType.ANNOTATION, value = Component.class),
                 @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
                 @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
-
         })
 public class ApplicationConfiguration {
 
@@ -32,22 +32,21 @@ public class ApplicationConfiguration {
 
         @Bean
         public ConnectionPool pool3() {
-                return new ConnectionPool( "test-pool", 25);
+                return new ConnectionPool("test-pool", 25);
         }
 
         @Bean
+        @Profile("prod|web")
+//    ! & |
         public UserRepository userRepository2(ConnectionPool pool2) {
                 return new UserRepository(pool2);
         }
 
         @Bean
         public UserRepository userRepository3() {
-                ConnectionPool connectionPool1 = pool3();
-                ConnectionPool connectionPool2 = pool3();
-                ConnectionPool connectionPool3 = pool3();
-
+                var connectionPool1 = pool3();
+                var connectionPool2 = pool3();
+                var connectionPool3 = pool3();
                 return new UserRepository(pool3());
         }
-
-
 }
